e71507d9ab61563b87e888e307bf1093a9e22d6b
JBrow95 <browningjustin95@gmail.com>
2018-08-09 18:58:43 -0400

    test failed chars are allowed

--- isbn.rb
+++ isbn.rb
@@ -1,10 +1,14 @@
 def valid_isbn_10(isbn)
 	isbn_arr = isbn.split("")
 	isbn_arr.delete_if {|n| n==' ' || n=='-'}
-	p "#{isbn_arr}"
+
+	isbn_checksum = 0
+
 	if isbn_arr.length == 10
 		true
 	else
 		false
 	end
+
+	# isbn_checkdigit = isbn_arr.pop
 end
--- isbn_tdd.rb
+++ isbn_tdd.rb
@@ -18,4 +18,8 @@ class TestIsbn < Minitest::Test
 	def test_if_isbn10_is_valid_with_space_and_dashes
 		assert_equal(true, valid_isbn_10("0431 958697"))
 	end
+
+	def test_if_isbn10_is_invalid_if_has_chars
+		assert_equal(false, valid_isbn_10("0431958a97"))
+	end
 end
