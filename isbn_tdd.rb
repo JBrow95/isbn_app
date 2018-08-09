a3566ffbad258ddc39f87e207e1310c2b91dc4c7
JBrow95 <browningjustin95@gmail.com>
2018-08-09 16:09:59 -0400

    test failed for class array

--- isbn_tdd.rb
+++ isbn_tdd.rb
@@ -4,7 +4,10 @@ require_relative "isbn.rb"
 class TestIsbn < Minitest::Test
 
 	def test_if_working
-		assert_equals(1, 1)
+		assert_equal(1, 1)
 	end
 
+	def test_if_class_is_arr
+		assert_equal(Array, valid().class)
+	end
 end
