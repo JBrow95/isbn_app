8a040c276cdd0ef17545b677c9bdcb86d3d63b18
Jbrown95 <browningjustin95@gmail.com>
2018-08-22 15:22:48 -0400

    finished aws s3

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@ source "https://rubygems.org"
 ruby '2.5.1'
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem 'sinatra'
-# gem 'aws-sdk'
+gem 'aws-sdk-s3'
--- app.rb
+++ app.rb
@@ -1,12 +1,15 @@
 require 'sinatra'
 require_relative 'isbn.rb'
 require 'csv'
-# require 'aws-sdk'
+require 'aws-sdk-s3'
+
+load 'local_ENV.rb' if File.exist?('local_ENV.rb')
 
 enable :sessions
 
+s3 = Aws::S3::Client.new(profile: 'Jbrow95', region: 'us-east-1')
+
 get '/' do
-	# arr_isbn = ""
 	nums = []
 	final_arr = []
 	erb :dashboard, locals:{final_arr: final_arr, nums: nums}
@@ -16,27 +19,29 @@ post '/validate' do
 	nums = params[:isbn_nums].split("\r\n") 
 	final_arr = []
 
-	# nums.each do |i|
-	# 	if i != ""
-	# 		final_arr << isbn_check(i)
-	# 	end
-	# end 
-	# final_arr
+	nums.each do |i|
+		if i != ""
+			final_arr << isbn_check(i)
+		end
+	end 
+	final_arr
 
 	numbers_arr = []
-	CSV.foreach("csv/output1.csv", ) do |row|
+	CSV.foreach("csv/input.csv", ) do |row|
 		numbers_arr << row[1]
 	end
 
 
 
-	CSV.open("csv/input_isbn_file.csv", "wb") do |csv|
+	temp = CSV.generate("output_isbn_file.csv") do |csv|
 		csv << ["ISBN", "CHECK"]
 		numbers_arr.shift
 			numbers_arr.each do |num|
 			csv << [num, isbn_check(num)]
 		end
 	end
+
+	s3.put_object(bucket: 'isbn-mm', body: temp, key: 'output_isbn_file.csv')
 	
 	erb :dashboard, locals:{final_arr: final_arr, nums: nums}
 end
